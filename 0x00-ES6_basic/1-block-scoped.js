58485260a0b6265c45d7aaa9006187d2eb1c6d99
Joshua <atsujoshua67@gmial.com>
2025-03-21 11:43:32 -0700

    update unused variable

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,10 +2,11 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
+  /* eslint-disabled no-unused-vars */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
-
+  /* eslint-enabled no-unused-vars */
   return [task, task2];
 }
