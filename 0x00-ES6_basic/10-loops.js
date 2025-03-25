eb6320ff469f5a3c512eb7cc095391aa906d286d
Joshua <atsujoshua67@gmial.com>
2025-03-24 18:52:14 -0700

    Iterator

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const arrayAppend = []
- for (const value of array) {
-   arrayAppend.push(`${appendString}${value}`);
- }
+  const arrayAppend = [];
+  for (const value of array) {
+    arrayAppend.push(`${appendString}${value}`);
+  }
 
   return arrayAppend;
 }
