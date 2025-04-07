d6a3c3be619b06b9b92d9941fc0746eb923361c1
Joshua <atsujoshua67@gmail.com>
2025-04-07 03:57:20 -0700

    promise handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,9 @@
 /* eslint-disable */
 export default function handleResponseFromAPI(promise) {
   const myPromise = new Promise((resolve, reject) => {
-    const success = 'Got a response from the API';
     resolve({
       status: 200,
-      body: success,
+      body: 'success',
     });
     reject(new Error());
   });
